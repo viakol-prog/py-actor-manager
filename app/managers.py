43754751ac5e41c732d8686fb575bfa10a18715d
Viacheslav Kolesnyk <viakol.lokaiv@gmail.com>
2026-05-05 14:24:53 +0300

    actor-manager

--- app/managers.py
+++ app/managers.py
@@ -10,19 +10,29 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         # Використовуємо f-рядок тільки для назви таблиці
-        query = f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)"
+        query = (
+            f"INSERT INTO {self.table_name} "
+            f"(first_name, last_name) VALUES (?, ?)"
+        )
         self.connection.execute(query, (first_name, last_name))
         self.connection.commit()
 
     def all(self) -> list[Actor]:
-        query = f"SELECT id, first_name, last_name FROM {self.table_name}"
+        query = (
+            f"SELECT id, first_name, last_name "
+            f"FROM {self.table_name}"
+        )
         cursor = self.connection.execute(query)
         rows = cursor.fetchall()
         # Повертаємо список об'єктів Actor
         return [Actor(id=row[0], first_name=row[1], last_name=row[2]) for row in rows]
 
     def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
-        query = f"UPDATE {self.table_name} SET first_name = ?, last_name = ? WHERE id = ?"
+        query = (
+            f"UPDATE {self.table_name} "
+            f"SET first_name = ?, last_name = ? "
+            f"WHERE id = ?"
+        )
         self.connection.execute(query, (new_first_name, new_last_name, pk))
         self.connection.commit()
 
